e413f10aee44513bb50599976b6ee14bf7735d99
ricar <ricardosr4@gmail.com>
2024-06-28 22:50:39 -0400

    xml register parte 2 ok

--- app/src/main/java/com/example/ecommerceapp/ui/login/presenter/LoginActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/ui/login/presenter/LoginActivity.kt
@@ -1,21 +1,24 @@
 package com.example.ecommerceapp.ui.login.presenter
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.ecommerceapp.R
+import com.example.ecommerceapp.databinding.ActivityLoginBinding
+import com.example.ecommerceapp.ui.register.presenter.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navigateToRegister()
+    }
+    private fun navigateToRegister(){
+        binding.tvTitleRegister.setOnClickListener {
+            val registerIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(registerIntent)
         }
     }
 }
--- app/src/main/java/com/example/ecommerceapp/ui/register/presenter/RegisterActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/ui/register/presenter/RegisterActivity.kt
@@ -1,21 +1,27 @@
 package com.example.ecommerceapp.ui.register.presenter
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.ecommerceapp.R
+import com.example.ecommerceapp.databinding.ActivityRegisterBinding
+import com.example.ecommerceapp.ui.login.presenter.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navigateToLogin()
+
+    }
+
+    private fun navigateToLogin() {
+        binding.titleDirectLogin.setOnClickListener {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
         }
+
     }
 }
